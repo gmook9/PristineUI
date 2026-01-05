ceb492aba0530ccb99ef044949f210b54f22d407
G <82985746+gmook9@users.noreply.github.com>
2026-01-04 20:38:07 -0800

    export comps

--- packages/pristineui/src/index.ts
+++ packages/pristineui/src/index.ts
@@ -1,3 +1,15 @@
 export { Button } from "./components/button";
 export type { ButtonProps } from "./components/button";
+
+export { Card, CardHeader, CardContent, CardFooter } from "./components/card";
+export type {
+  CardProps,
+  CardHeaderProps,
+  CardContentProps,
+  CardFooterProps,
+} from "./components/card";
+
+export { Input } from "./components/input";
+export type { InputProps } from "./components/input";
+
 export { cn } from "./utils/cn";
