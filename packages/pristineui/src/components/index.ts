0b6c6e0a9be34d19df45fdaf45703d8b9330114a
G <82985746+gmook9@users.noreply.github.com>
2026-01-04 20:46:08 -0800

    standardized component exports via barrel files

---
+++ packages/pristineui/src/components/index.ts
@@ -0,0 +1,13 @@
+export { Button } from "./button";
+export type { ButtonProps } from "./button";
+
+export { Card, CardHeader, CardContent, CardFooter } from "./card";
+export type {
+  CardProps,
+  CardHeaderProps,
+  CardContentProps,
+  CardFooterProps,
+} from "./card";
+
+export { Input } from "./input";
+export type { InputProps } from "./input";
--- packages/pristineui/src/index.ts
+++ packages/pristineui/src/index.ts
@@ -1,15 +1,2 @@
-export { Button } from "./components/button";
-export type { ButtonProps } from "./components/button";
-
-export { Card, CardHeader, CardContent, CardFooter } from "./components/card";
-export type {
-  CardProps,
-  CardHeaderProps,
-  CardContentProps,
-  CardFooterProps,
-} from "./components/card";
-
-export { Input } from "./components/input";
-export type { InputProps } from "./components/input";
-
+export * from "./components";
 export { cn } from "./utils/cn";
